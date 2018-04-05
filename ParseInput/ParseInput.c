9a7dfdfdf8797491bec5f612a8a189a338368871
GusGu3 <38086684+GusGu3@users.noreply.github.com>
2018-04-05 15:01:21 -0400

    Update ParseInput.c

--- ParseInput/ParseInput.c
+++ ParseInput/ParseInput.c
@@ -15,30 +15,28 @@ int main()
     int strLength = 0; /* temporary int for the length of the string */
     int numZero = 0; /* int to record the number of zeros */
     int numOne = 0; /* int to record the number of ones */
+
+    strLength = strlen(input);
     
-    while ( gets(input) )   /* gets returns NULL at end-of-file */
+    /* go through the string and count the 0s and 1s */
+    int i;
+    for (i = 0; i < strLength; i = i+1)
     {
-        strLength = strlen(input);
-        
-        /* go through the string and count the 0s and 1s */
-        int i;
-        for (i = 0; i < strLength; i = i+1)
+        if (input[i] == 0)
         {
-            if (input[i] == 0)
-            {
-                numZero = numZero + 1;
-                printf("zeros: %d", numZero);
-            }
-            else if (input[i] == 1)
-            {
-                numOne = numOne + 1;
-            }
-        }
-        
-        /* print the list size and the number of 0s and 1s */
-        printf("The list size is: %d. \n The number of zeros is: %s. \nThe number of ones is: %d.", strLength, numZero, numOne);
-        
-        /* Set the temporary string length variable back to zero */
-        strLength = 0;
-    }
-}
+            numZero = numZero + 1;
+            //printf("zeros: %d", numZero);
+        }//end if
+        else if (input[i] == 1)
+        {
+            numOne = numOne + 1;
+        }//end else if
+    }//end for
+       
+    /* print the list size and the number of 0s and 1s */
+    printf("The list size is: %d. \n The number of zeros is: %s. \nThe number of ones is: %d.", strLength, numZero, numOne);
+      
+    /* Set the temporary string length variable back to zero */
+    strLength = 0;
+
+}//end main
