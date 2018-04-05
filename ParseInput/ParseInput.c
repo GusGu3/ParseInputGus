e07d0cdb934122676ed2749eff3e7482d6ae5827
GusGu3 <38086684+GusGu3@users.noreply.github.com>
2018-04-05 14:37:56 -0400

    Created by Coding Ground Client

---
+++ ParseInput/ParseInput.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+Name: Parse Input
+Author: Gus Guthrie
+Version: 4/5/2018
+Description:
+*/
+
+int main()
+{
+    /* Create array of characters */
+    char input[BUFSIZ];
+    int strLength = 0; /* temporary int for the length of the string */
+    int numZero = 0; /* int to record the number of zeros */
+    int numOne = 0; /* int to record the number of ones */
+    
+    while ( gets(input) )   /* gets returns NULL at end-of-file */
+    {
+        strLength = strlen(input);
+        
+        /* go through the string and count the 0s and 1s */
+        int i;
+        for (i = 0; i < strLength; i = i+1)
+        {
+            if (input[i] == 0)
+            {
+                numZero = numZero + 1;
+                printf("zeros: %d", numZero);
+            }
+            else if (input[i] == 1)
+            {
+                numOne = numOne + 1;
+            }
+        }
+        
+        /* print the list size and the number of 0s and 1s */
+        printf("The list size is: %d. \n The number of zeros is: %s. \nThe number of ones is: %d.", strLength, numZero, numOne);
+        
+        /* Set the temporary string length variable back to zero */
+        strLength = 0;
+    }
+}
